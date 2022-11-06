fe6f2203c6fe26db1030d3f754ff6cb4e1d1bc9e
Carmen Amba <carmieamba@gmail.com>
2022-11-06 17:00:34 +0100

    Updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,23 +11,23 @@ const Home = () => {
         ‍<title>Pets - Products</title>‍
         <meta
           name="description"
-          content="Find all the best quality products your pet may need"
+          content="It is a simple voting platform, it has a unique user experience and a good user interface as well. link:https://carmenbelle.netlify.app/"
         />
-        <meta name="voting-card" content="summary_large_image" />
-        <meta name="voting:site" content="@user" />
+        <meta name="voting-platform" content="summary_large_image" />
+        <meta name="voting:platform" content="@user" />
         <meta name="voting:creator" content="@user" />
-        <meta name="vote:title" content="Pets - Products" />
+        <meta name="vote:title" content="votingsite - Products" />
         <meta
-          name="Online-voting:description"
-          content="Best Products for your pet"
+          name="Online-voting platform:description"
+          content="Best Products for your voting platform."
         />
-        <meta name="oline-voting:image" content="url_to_image" />
+        <meta name="oline-voting platform" content="voting platform" />
         <meta property="og:title" content="election" />
-        <meta property="og:description" content="Best Products for your pet" />
-        <meta property="og:url" content="voting.abc" />
-        <meta property="og:site_name" content="votes - Products" />
+        <meta property="og:description" content="Best Products for your voting platform" />
+        <meta property="og:url" content="https://carmenbelle.netlify.app/" />
+        <meta property="og:voting-App" content="voting platform - Products" />
         <meta property="og:locale" content="en_US" />
-        <meta property="og:type" content="article" />
+        <meta property="og:type" content="A simple voting platform, it has a unique user experience and a good user interface as well" />
         <meta property="voting:app_id" content="ID_APP_VOTE" />
       </Helmet>
       <FaAirbnb style={{ width: "20%", height: "30px", color: "grey" }} />
